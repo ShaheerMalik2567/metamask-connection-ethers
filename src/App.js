4a51fd08368913f4afabb3a68bb0a5c495d41e8b
shaheermalik2567 <shaheermalik.2567@gmail.com>
2022-10-04 14:53:37 +0500

    ethers

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+import MetamaskConnect from "./components/Connection";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.App}>
+      <header className={styles["App-header"]}>
+        <img src={logo} className={styles["App-logo"]} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles["App-link"]}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <br />
+        <div>
+          <MetamaskConnect />
+        </div>
       </header>
     </div>
   );
---
+++ src/components/Connection.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { ethers } from "ethers";
+import { Card } from "react-bootstrap";
+
+const MetamaskConnect = () => {
+  const [data, setdata] = useState({
+    address: "",
+    Balance: null,
+  });
+
+  // Button handler button for handling a
+  // request event for metamask
+  const btnhandler = () => {
+    // Asking if metamask is already present or not
+    if (window.ethereum) {
+      // res[0] for fetching a first wallet
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((res) => accountChangeHandler(res[0]));
+    } else {
+      alert("install metamask extension!!");
+    }
+  };
+
+  // getbalance function for getting a balance in
+  // a right format with help of ethers
+  const getbalance = (address) => {
+    // Requesting balance method
+    window.ethereum
+      .request({
+        method: "eth_getBalance",
+        params: [address, "latest"],
+      })
+      .then((balance) => {
+        // Setting balance
+        setdata({
+          Balance: ethers.utils.formatEther(balance),
+        });
+      });
+  };
+
+  // Function for getting handling all events
+  const accountChangeHandler = (account) => {
+    // Setting an address data
+    setdata({
+      address: account,
+    });
+
+    // Setting a balance
+    getbalance(account);
+  };
+  return (
+    <div className="App">
+      {/* Calling all values which we 
+           have stored in usestate */}
+
+      <Card className="text-center">
+        <Card.Body>
+          <Card.Text>
+            <strong>Address: </strong>
+            {data.address}
+          </Card.Text>
+          <Card.Text>
+            <strong>Balance: </strong>
+            {data.Balance}
+          </Card.Text>
+          <button className="button" onClick={btnhandler} variant="primary">
+            Connect to wallet
+          </button>
+          <button className="button">disconnect</button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default MetamaskConnect;
